3ab09540a644f5c04b3a9c62117dbaeff52db595
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-03 21:59:33 +0000

    poc2: trying some stuff and finding limitations

--- poc2.cpp
+++ poc2.cpp
@@ -8,7 +8,15 @@ import jute;
 import print;
 
 static void nav(TSTreeCursor & c) {
-  putln("enter ", ts_node_string(ts_tree_cursor_current_node(&c)));
+  auto node = ts_tree_cursor_current_node(&c);
+  auto type = jute::view::unsafe(ts_node_type(node));
+  if (type == "preproc_call") {
+    auto dir = ts_node_child_by_field_name(node, "directive", 9);
+    putln("pp call ", ts_node_child_count(dir));
+    return;
+  }
+
+  putln("enter ", ts_node_string(node));
   if (ts_tree_cursor_goto_first_child(&c)) {
     nav(c);
     while (ts_tree_cursor_goto_next_sibling(&c)) {
@@ -22,7 +30,9 @@ static void nav(TSTreeCursor & c) {
 int main() {
   glen::parser p { tree_sitter_cpp };
   auto t = p.parse(jute::view {
+    // Note: tree-sitter does not distinguish between #ifdef and #ifndef
     "#ifdef DEBUG\n"
+    // Note: we can, on a best case, get "#  pragma" as the "call type"
     "#  pragma leco tool\n"
     "#else\n"
     "#  error\n"
